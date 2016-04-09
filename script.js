3beb0d0136d76a0657fd4bd83a691ecfa562ef53
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-04-08 23:45:03 -0700

    added sound effects

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 //add a click handler to each card using jquery(chosen) or onclick-attribute
 $(function () {  //document ready function
     $('.card').click(function () {
+        folsom.play();
+        folsom.volume=0.3;
         randomize();
         card_clicked(this);
         return(card_clicked);
     });
+
     $('.reset').click(function () {  //declared reset button function
+        $('.card').find('.back').show();
+        $('.gameover').hide();
         games_played++;
         reset_stats();
         display_stats();
-        remove_shuffle()
+        remove_shuffle();
         randomize();
-
-        $('.gameover').hide();
-        $('.card').find('.back').show();
+        police.play();
+        folsom.pause();
+        escape.pause();
+        escape.currentTime = 0;
     });
 });
 
@@ -27,6 +33,13 @@ var attempts = 0;
 var accuracy = 0;
 var games_played = 0;
 var random_mug = [];
+
+//sounds
+var police = new Audio('audio/openup.mov');
+var escape = new Audio('audio/escape.mp3');
+var folsom = new Audio('audio/FolsomPrisonBlues.mp3');
+var door = new Audio('audio/door.mov');
+var robo = new Audio('audio/trouble.mov');
 
 
 function randomize() {
@@ -64,7 +77,6 @@ function randomize() {
         $('.card:nth-child(' + (j + 1) + ')').prepend('<div class="front"><img src="' + random_mug[j] + '"></div>');
     }
 }
-console.log(random_mug);
 
 function card_clicked(das_card) {
     if (!user_click) {          //added a flag variable for the purpose of denying a user's ability to click more than 2 cards
@@ -82,6 +94,7 @@ function card_clicked(das_card) {
 
         //if statement to check if first and second cards clicked are equal
         if (first_card_clicked.find('.front > img').attr('src')== second_card_clicked.find('.front > img').attr('src')) {
+            robo.play();
             matches++; // if card match then match_counter is incremented
             compute_accuracy(); // calls upon compute_accuracy function to compute and display accuracy percentage
             display_stats();  //update stats display
@@ -89,17 +102,19 @@ function card_clicked(das_card) {
             second_card_clicked = null;
             if (matches == total_possible_matches) {
                 $('.gameover').show();
+                escape.play();
+                folsom.pause();
             }
         } else {
 
-            //dan start
+            door.play();
             user_click = false; // set user_click to false to check if condition changes in function
             compute_accuracy(); //calls compute_accuracy function which performs math to display accuracy on stat bar in the game
             display_stats(); //calls display_stats function which relays updated stat information
             //sets the unmatched cards to show the back again after two seconds
             var timerId = setTimeout(function () { //declares function inside variable timerId
-                $(first_card_clicked).find('.back').show(); //displays the back card after 2 seconds
-                $(second_card_clicked).find('.back').show(); //displays the back  card after 2 seconds
+                $(first_card_clicked).find('.back').show(); //displays the back card after .8 seconds
+                $(second_card_clicked).find('.back').show(); //displays the back  card after .8 seconds
                 first_card_clicked = null; //reset first_card_clicked to null
                 second_card_clicked = null;//reset second_card_clicked to null
                 user_click = true; //set the flag variable to true when condition changed
