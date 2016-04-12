47e45f86a8b209f6c8618ec1f36768136d4c7e0d
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-04-12 00:11:10 -0700

    updated card flip animation

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //add a click handler to each card using jquery(chosen) or onclick-attribute
 $(function () {  //document ready function
+    randomize();
     $('.card').click(function () {
         folsom.play();
         folsom.volume=0.3;
-        randomize();
         card_clicked(this);
-        return(card_clicked);
     });
 
     $('.reset').click(function () {  //declared reset button function
@@ -98,13 +97,13 @@ function card_clicked(das_card) {
     });
     //checks if first card was clicked
     if (first_card_clicked == null) { //check if first_card_clicked == null
-        first_card_clicked = $(das_card)
+        first_card_clicked = $(das_card);
     } else {
         second_card_clicked = $(das_card);
         attempts++;
 
         //if statement to check if first and second cards clicked are equal
-        if (first_card_clicked.find('.front > img').attr('src')== second_card_clicked.find('.front > img').attr('src')) {
+        if (first_card_clicked.find('.front > img').attr('src') === second_card_clicked.find('.front > img').attr('src')) {
             robo.play();
             matches++; // if card match then match_counter is incremented
             compute_accuracy(); // calls upon compute_accuracy function to compute and display accuracy percentage
@@ -124,9 +123,7 @@ function card_clicked(das_card) {
             compute_accuracy(); //calls compute_accuracy function which performs math to display accuracy on stat bar in the game
             display_stats(); //calls display_stats function which relays updated stat information
             //sets the unmatched cards to show the back again after two seconds
-            var timerId = setTimeout(function () { //declares function inside variable timerId
-                //$(first_card_clicked).find('.back').show(); //displays the back card after .8 seconds
-                //$(second_card_clicked).find('.back').show(); //displays the back  card after .8 seconds
+            var timerId = setTimeout(function () {
                 second_card_clicked.find('.back').css({
                     "transform": "rotateY(0)",
                     "transform-style": "preserve-3d",
@@ -145,7 +142,7 @@ function card_clicked(das_card) {
                     "transition": "transform .2s linear 0s",
                     "transition": "-webkit-transform .2s linear 0s"
                 });
-                first_card_clicked.find('front').css({
+                first_card_clicked.find('.front').css({
                     "transform": "rotateY(-180deg)",
                     "transform-style": "preserve-3d",
                     "transition": "transform .2s linear 0s",
